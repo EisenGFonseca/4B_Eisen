7d0f0b7d03423d1eb19704b563e71f5d65be44d8
Dr <Dr@Eisen>
2018-05-04 14:54:03 -0400

    Criando MenuBar

--- EisenFonseca_4b/src/br/edu/ifro/MenuPrincipalController.java
+++ EisenFonseca_4b/src/br/edu/ifro/MenuPrincipalController.java
@@ -7,12 +7,16 @@ package br.edu.ifro;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
@@ -45,5 +49,25 @@ public class MenuPrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void AbrirCadastroAluno(ActionEvent event) {
+    try{
+        FXMLLoader fxmlLoader = new FXMLLoader();
+    fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(),900,682);
+    Stage stage = new Stage();
+    stage.setTitle("Cadastrar Aluno");
+    stage.setScene(scene);
+    stage.show();
+}
+catch(IOException e){
+
+}
+    }
+
+    @FXML
+    private void FecharCadastroAluno(ActionEvent event) {
+    }
     
 }
