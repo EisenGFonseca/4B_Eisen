eac398f69723d8df74ff4c3f7762cfe2b0f76b9b
Dr <Dr@Eisen>
2018-05-11 14:19:53 -0400

    Finalização

--- EisenFonseca_4b/src/br/edu/ifro/AlunoController.java
+++ EisenFonseca_4b/src/br/edu/ifro/AlunoController.java
@@ -5,9 +5,19 @@
  */
 package br.edu.ifro;
 
+import br.edu.ifro.Modelo.Aluno;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.swing.WindowConstants;
 
 /**
  * FXML Controller class
@@ -16,6 +26,13 @@ import javafx.fxml.Initializable;
  */
 public class AlunoController implements Initializable {
 
+    @FXML
+    private JFXTextField txtAluno;
+    @FXML
+    private JFXButton cadastrarAluno;
+    @FXML
+    private JFXButton btcancelar;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +40,28 @@ public class AlunoController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void CadAluno(ActionEvent event) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("aula");
+        EntityManager em = emf.createEntityManager();
+        
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome(txtAluno.getText());
+        
+        em.getTransaction().begin();
+        
+        em.persist(aluno1);
+        
+        em.getTransaction().commit();
+         
+
+    }
+
+    @FXML
+    private void Cancelar(ActionEvent event) {
+    Stage stage = (Stage) btcancelar.getScene().getWindow();
+    stage.close();
+    }
     
 }
